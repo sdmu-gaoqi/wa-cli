1313f624abd3883685dcb714ec9e85465f9eedc3
gaoqi <1224362143@qq.com>
2025-02-06 19:06:56 +0800

    feat: xlsx转换

--- config/command.js
+++ config/command.js
@@ -1,4 +1,5 @@
 const init = require("../lib/init");
+const xlsx2Config = require("../lib/locale/xlsx2Config");
 
 const command = [
   {
@@ -8,6 +9,35 @@ const command = [
     action: (name) => {
       init(name);
     },
+  },
+  {
+    name: "xlsx2Config [path] [name] [ext]",
+    alias: "l",
+    description: "xlsx文件转多语言配置",
+    positional: [
+      {
+        key: "path",
+        option: {
+          describe: "文件路径",
+          type: "string",
+        },
+      },
+      {
+        key: "name",
+        option: {
+          describe: "xlsx文件名称",
+          type: "string",
+        },
+      },
+      {
+        key: "ext",
+        option: {
+          describe: "转换文件格式(ts or json)",
+          type: "string",
+        },
+      },
+    ],
+    action: xlsx2Config,
   },
 ];
 
